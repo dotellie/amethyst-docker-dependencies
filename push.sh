c1a4fa94036da7526d1f85fa3bebbc2a2ee7d006
Ellie <hi@ellie.moe>
2018-10-18 21:50:35 +0200

    Fix pushing

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push magnonellie/amethyst-docker-cache
+docker push magnonellie/amethyst-dependencies
